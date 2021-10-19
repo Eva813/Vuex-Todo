17363be41d13135fad046d837ea52d36c4e11b30
Eva813 <as45986@gmail.com>
2021-10-19 16:51:07 +0800

    get axios data from vuex

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,4 +1,19 @@
-<style>
+<style lang="scss" scoped>
+.todos {
+  display: grid;
+  //使用grid 排版
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #ef9a9a;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
 </style>
 <template>
   <h3>Todos</h3>
@@ -10,10 +25,16 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Todos",
+  methods: {
+    ...mapActions(["fetchTodos"]),
+  },
   computed: mapGetters(["getTodos"]),
+  created() {
+    this.fetchTodos();
+  },
 };
 </script>
 
