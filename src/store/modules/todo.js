025011e3e7b8a0606dd13b65f4eb003cf9fa4593
Eva813 <as45986@gmail.com>
2021-10-19 21:26:33 +0800

    add post data

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -10,12 +10,20 @@ const actions = {
   async fetchTodos({ commit }) {
     const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
     // console.log(res.data);
-    commit('setTodos', res.data)
+    commit('setTodos', res.data);
+  },
+  async addTodo({ commit }, title) {
+    const res = await axios.post("https://jsonplaceholder.typicode.com/todos", { title, completed: false });
+    commit('addNewTodo', res.data);
   }
+
 };
 const mutations = {
   setTodos: (state, todos) => {
     state.todos = todos;
+  },
+  addNewTodo: (state, todo) => {
+    state.todos.unshift(todo);
   }
 };
 
