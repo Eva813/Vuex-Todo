f16c7c9a79d6bb3bc4ec6e5099bf32e154ef74a3
Eva813 <as45986@gmail.com>
2021-10-19 15:31:59 +0800

    get todo array and v-for it

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,20 @@
+<style>
+</style>
 <template>
   <h3>Todos</h3>
+  <div class="todos">
+    <div class="todo" v-for="todo in getTodos" :key="todo.id">
+      {{ todo.title }}
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "Todos",
+  computed: mapGetters(["getTodos"]),
 };
 </script>
 
-<style>
-</style>
+
