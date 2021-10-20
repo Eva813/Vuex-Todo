6c6cb75788b5ef75697ee6be9225a5dfd1446edf
Eva813 <as45986@gmail.com>
2021-10-20 14:03:49 +0800

    modify background color

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,31 @@
 body {
   font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   line-height: 1.6;
-  background: #fff4d3;
+  // background: linear-gradient(135deg, #d88425 50%, #fff 50%);
+  background-image: repeating-linear-gradient(
+    135deg,
+    //↘
+    #fff4d3 /*0px*/,
+    #f3d8ba 100px,
+    //100px
+    #fff4d3 100px,
+    #fff4d3 105px,
+    //5px
+    #f3d8ba 105px,
+    #f3d8ba 130px,
+    //25px
+    #fff4d3 130px,
+    #fff4d3 260px,
+    //130px
+    #fff4d3 260px,
+    #fff4d3 265px,
+    //5px
+    #fff4d3 265px,
+    #fff4d3 270px,
+    //5px
+    #fff4d3 270px,
+    #fff4d3 330px //60px
+  );
 }
 .container {
   max-width: 1100px;
@@ -16,6 +40,7 @@ body {
   <div id="app">
     <div class="container">
       <AddTodo />
+      <FilterTodos />
       <Todos />
     </div>
   </div>
@@ -27,12 +52,14 @@ body {
 // @ is an alias to /src
 import Todos from "@/components/Todos.vue";
 import AddTodo from "@/components/AddTodo.vue";
+import FilterTodos from "@/components/FilterTodos.vue";
 
 export default {
   name: "app",
   components: {
     Todos,
     AddTodo,
+    FilterTodos,
   },
 };
 </script>
