30515ad705baff6722573c31526de31ac69d8b44
Eva813 <as45986@gmail.com>
2021-10-20 14:04:54 +0800

    implement double click chenge the state of todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -25,11 +25,49 @@ i {
     color: #ca4f4f;
   }
 }
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #ef9a9a;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
 </style>
 <template>
   <h3>Todos</h3>
+  <div class="legend">
+    <span>雙重點擊來標示已完成的事項</span>
+    <span> <span class="incomplete-box"></span> = 未完成 </span>
+    <span> <span class="complete-box"></span> = 已完成 </span>
+  </div>
   <div class="todos">
-    <div class="todo" v-for="todo in getTodos" :key="todo.id">
+    <div
+      @dblclick="onDblclick(todo)"
+      class="todo"
+      v-for="todo in getTodos"
+      :key="todo.id"
+      :class="{ 'is-complete': todo.completed }"
+    >
       {{ todo.title }}
       <i class="fas fa-trash-alt" @click="deleteTodo(todo.id)"></i>
     </div>
@@ -41,7 +79,15 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Todos",
   methods: {
-    ...mapActions(["fetchTodos", "deleteTodo"]),
+    ...mapActions(["fetchTodos", "deleteTodo", "updataTodo"]),
+    onDblclick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed,
+      };
+      this.updataTodo(updTodo);
+    },
   },
   computed: mapGetters(["getTodos"]),
   created() {
